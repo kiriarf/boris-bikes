aa42fee18dd6e28f1d3668ce06223970c0ac812c
Dan Holmes <dan@Dans-MacBook-Air.local>
2020-09-30 16:39:36 +0100

    refactor ex 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,13 +4,21 @@ describe DockingStation do
   docking_station = DockingStation.new
   bike = docking_station.release_bike
 
-  it "bike released" do
-    expect(docking_station.bikes_docked.length).to eq 0
+  describe "bike released" do
+    it { expect(docking_station.bikes_docked.length).to eq 0 }
   end
 
-  it "bike docked" do
-    docking_station.dock(bike)
-    expect(docking_station.bikes_docked.length).to eq 1
-    expect(docking_station.bikes_docked[0]).to be_an_instance_of Bike
+  describe "bike docked" do
+    before do
+      docking_station.dock(bike)
+    end
+    it { expect(docking_station.bikes_docked.length).to eq 1 }
+    it { expect(docking_station.bikes_docked[0]).to be_an_instance_of Bike }
   end
+
+  # it "doesn't give out bikes that don't exist" do
+  #   if docking_station.bikes_docked.length == 0
+  #     expect(docking_station.release_bike) #throws error
+  #   end
+  # end
 end
