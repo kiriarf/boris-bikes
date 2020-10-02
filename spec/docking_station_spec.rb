b1d96ebb4dbf40d97e9af7e080c87d4fd6a2291d
Kiril <kiril@olgas-air.mynet>
2020-10-02 17:42:00 +0100

    finished exercise 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'DockingStation'
 
 describe DockingStation do
   let(:ds) {DockingStation.new}
-  let(:bike) {Bike.new}
+  let(:bike) {double :bike}
   
   it "has no bikes when initialized" do
     expect(ds.bikes_docked).to be_empty
@@ -19,6 +19,7 @@ describe DockingStation do
   end
 
   it "can release a bike" do
+    allow(bike).to receive(:working?).and_return(true)
     ds.dock(bike)
     ds.release_bike
     expect(ds.bikes_docked).not_to include(bike)
@@ -29,7 +30,9 @@ describe DockingStation do
   end
 
   it "does not release broken bikes" do
+    allow(bike).to receive(:working?).and_return(false)
     ds.dock(bike)
+    allow(bike).to receive(:report_broken).and_return(true)
     bike.report_broken
     expect { ds.release_bike }.to raise_error("All bikes broken.")
   end
